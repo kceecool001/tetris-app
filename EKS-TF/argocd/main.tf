eb38bc87745d740ab4268e30d6bc5655264970ae
kceedev <kceedev@outlook.com>
2026-01-31 19:50:45 +0200

    argocd set up

---
+++ EKS-TF/argocd/main.tf
@@ -0,0 +1,21 @@
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "6.7.2"
+
+  create_namespace = true
+
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
